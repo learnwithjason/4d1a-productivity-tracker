1d66c85887a988608f20fe94d6278f9cf0ec0f77
Jason Lengstorf <jason@lengstorf.com>
2023-10-09 08:35:17 -0700

    chore: cleanup

--- src/client/auth.ts
+++ src/client/auth.ts
@@ -2,49 +2,43 @@ import Clerk from '@clerk/clerk-js';
 
 const clerk = new Clerk(import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY);
 
-console.log('?');
-
 await clerk.load();
 
 async function updateUI() {
-  if (clerk.user) {
-    const targetDiv = document.querySelector<HTMLDivElement>('.clerk');
+	if (clerk.user) {
+		const targetDiv = document.querySelector<HTMLDivElement>('.clerk');
 
-    if (!targetDiv) {
-      return;
-    }
+		if (!targetDiv) {
+			return;
+		}
 
-    console.log('clerk.user');
-    clerk.mountUserButton(targetDiv, {
-      afterSignOutUrl: '/',
-      appearance: {
-        elements: {
-          avatarBox: {
-            aspectRatio: 1,
-            border: '1px solid var(--color-border)',
-            height: 'auto',
-            width: 'clamp(30px, 6.5vw, 50px)',
-          },
-        },
-      },
-    });
-    return;
-  }
+		clerk.mountUserButton(targetDiv, {
+			afterSignOutUrl: '/',
+			appearance: {
+				elements: {
+					avatarBox: {
+						aspectRatio: 1,
+						border: '1px solid var(--color-border)',
+						height: 'auto',
+						width: 'clamp(30px, 6.5vw, 50px)',
+					},
+				},
+			},
+		});
+		return;
+	}
 
-  console.log('hi');
+	// not logged in; show a sign in button
+	const url = await clerk.buildSignInUrl({ redirectUrl: '/auth-redirect' });
+	const btns = document.querySelectorAll('[data-clerk-login]');
 
-  // not logged in; show a sign in button
-  const url = await clerk.buildSignInUrl({ redirectUrl: '/auth-redirect' });
-  const btns = document.querySelectorAll('[data-clerk-login]');
+	btns.forEach((btn) => {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault();
 
-  btns.forEach((btn) => {
-    console.log(btn);
-    btn.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      window.location.href = url;
-    });
-  });
+			window.location.href = url;
+		});
+	});
 }
 
 updateUI();
