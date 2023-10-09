95ee496e5ece30e3cfba10a5384e83a9da717b2b
Jason Lengstorf <jason@lengstorf.com>
2023-10-09 08:53:55 -0700

    fix: don’t crash with no settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify()
+	output: 'server',
+	adapter: netlify(),
 });
