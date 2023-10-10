baa87285884a60168182f56e3f90bcdc297bfca6
panteliselef <panteliselef@outlook.com>
2023-10-10 13:39:06 +0300

    chore: remove `FLAG_GUESTBOOK` env reference

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,7 +9,6 @@ interface ImportMetaEnv {
 	readonly CLERK_JWT_KEY?: string;
 	readonly CLERK_SECRET_KEY?: string;
 	readonly DATABASE_URL?: string;
-	readonly FLAG_GUESTBOOK?: string;
 }
 
 interface ImportMeta {
